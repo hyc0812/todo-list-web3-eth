62d4a8ce486d494062162128abd93ebb26ae1b5c
yongchang he <hyc0812@163.com>
2022-05-31 13:48:03 -0600

    added

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-
 contract TodoList {
-    
     struct Task {
         uint id;
         string content;
         bool completed;
     }
-
     event TaskCreated(uint id, string content, bool completed);
     event TaskCompleted(uint id, bool completed);
+    mapping (address => mapping(uint => Task)) public tasks;
 }
